c0022ceff22b5b766510197b6628dab3ce3c50f0
Luciano Faria <luciano15faria@gmail.com>
2025-10-25 17:04:25 -0300

    Procedures exercises

---
+++ exercicios-logica-programacao-py/procedimentos/HelloWorldEmbananado.py
@@ -0,0 +1,15 @@
+"""
+#86 Hello World embananado
+Crie um programa que tenha um procedimento Gerador() que, quando chamado,
+mostre a mensagem "Olá, Mundo!" com algum componente visual (linhas)
+Ex: Ao chamar Gerador() aparece:
++-------=======------+
+ Olá, Mundo!
++-------=======------+
+"""
+def Gerador():
+    print("+-----========-----+")
+    print("Hello World")
+    print("------========-----+")
+
+Gerador()
