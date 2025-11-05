3d0e2af1a5e143e03fec55a8ceeddde14acb33d0
Luciano Faria <luciano15faria@gmail.com>
2025-11-05 18:18:00 -0300

    function exercise

---
+++ exercicios-logica-programacao-py/funcoes/functions.py
@@ -0,0 +1,12 @@
+"""
+Functions
+
+Crie uma função que imprime “Boa noite!”.
+
+Crie uma função que recebe um nome e imprime uma saudação."""
+
+def saudacao(name):
+    print("Boa noite, " + name)
+
+name = input("What's yout name? ")
+saudacao(name)
