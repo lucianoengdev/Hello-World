a6ca4766e49665424b7c76466ef32fe38139644e
Luciano Faria <luciano15faria@gmail.com>
2025-11-06 20:26:59 -0300

    neested loops exercise

---
+++ exercicios-logica-programacao-py/repeticao-para/nestedloops.py
@@ -0,0 +1,14 @@
+"""2D Lists & Nested Loops
+
+Crie uma matriz 3x3 e imprima todos os itens.
+
+Representar tabuleiro 3x3 e imprimir em formato tabela."""
+
+number = [ 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [9],
+]
+for n in number:
+    print(n)
