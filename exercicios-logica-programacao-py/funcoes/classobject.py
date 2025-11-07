22172627e796c3da95d7c2147c7295aafe81cda6
Luciano Faria <luciano15faria@gmail.com>
2025-11-07 18:22:49 -0300

    updating class exercise

--- exercicios-logica-programacao-py/funcoes/classobject.py
+++ exercicios-logica-programacao-py/funcoes/classobject.py
@@ -2,3 +2,7 @@ class Car:
     def __init__(self, car, year):
         self.car = car
         self.year = year
+
+    def age(self):
+        idade = 2025 - int(self.year)
+        return idade
--- exercicios-logica-programacao-py/funcoes/testingclass.py
+++ exercicios-logica-programacao-py/funcoes/testingclass.py
@@ -7,5 +7,7 @@ from classobject import Car
 carro1 = Car("Celta", "2008")
 carro2 = Car("Uno Quadrado", "2003")
 
-print(f"{carro1.car}, {carro1.year}")
-print(f"{carro2.car}, {carro2.year}")
+#print(f"{carro1.car}, {carro1.year}")
+#print(f"{carro2.car}, {carro2.year}")
+
+print(carro1.age())
