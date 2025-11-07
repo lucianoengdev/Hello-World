b40326556a186adeca26a2eccb66b642e3dd20da
Luciano Faria <luciano15faria@gmail.com>
2025-11-07 17:37:24 -0300

    class and object exercise

---
+++ exercicios-logica-programacao-py/funcoes/testingclass.py
@@ -0,0 +1,11 @@
+"""Classes & Objects
+Crie classe Carro com atributos marca e ano.
+Crie método que imprime descrição do carro.
+"""
+from classobject import Car
+
+carro1 = Car("Celta", "2008")
+carro2 = Car("Uno Quadrado", "2003")
+
+print(f"{carro1.car}, {carro1.year}")
+print(f"{carro2.car}, {carro2.year}")
