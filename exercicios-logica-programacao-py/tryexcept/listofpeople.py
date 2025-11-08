2e34d3af7aa9f4069ed08e31b34d573e90dbc221
Luciano Faria <luciano15faria@gmail.com>
2025-11-08 18:30:08 -0300

    Database for this exercise and developing

--- exercicios-logica-programacao-py/tryexcept/listofpeople.py
+++ exercicios-logica-programacao-py/tryexcept/listofpeople.py
@@ -1,6 +1,20 @@
 import sqlite3
 
-conn = sqlite3.connect('listapessoas.db')
+dtbase = 'listadepessoas.db'
+
+
+def criar_tabela():
+    conn = sqlite3.connect(dtbase)
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS pessoas( 
+                        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                        nome TEXT NOT NULL, 
+                        idade INTEGER NOT NULL
+                        );
+                        ''')
+    conn.close()
+
+criar_tabela()
 
 while True:
     print(f"--------------------------")
@@ -17,10 +31,34 @@ while True:
             print("Você não digitou uma das opções válidas (1, 2 ou 3)")
 
     if(opcao == 1):
-        print("Pessoas listadas: ")
+        conn = sqlite3.connect(dtbase)
+        cursor = conn.cursor()
+        cursor.execute("SELECT nome, idade FROM pessoas")
+        todas_pessoas = cursor.fetchall()
+        conn.close
+        if not todas_pessoas:
+            print("Nenhuma pessoa na lista")
+            print("--------------------------")
+            
+        else:
+            print("---Lista de pessoas cadastradas: ---")
+            for pessoa in todas_pessoas:
+                print(f"Nome: {pessoa[0]}   Idade: {pessoa[1]}")
+                print("--------------------------")
     
     elif(opcao == 2):
-        print("Você digitou a opção 2")
+        while True:
+            try:
+                n = str(input("Nome: "))
+                i = int(input("Idade: "))
+                break
+            except Exception as erro:
+                print(f"Você cometeu o erro {erro}")
+
+        conn = sqlite3.connect(dtbase)
+        cursor = conn.cursor()
+        cursor.execute("INSERT into pessoas, ")
+        print("--------------------------")
     
     else:
         print("Você saiu do programa")
